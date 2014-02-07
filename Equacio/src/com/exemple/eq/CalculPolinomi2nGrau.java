effbae3f4378bfccfa8bd2d86eaeab1fb163c881
Pau <Pau@Wokfly>
2014-02-07 19:28:36 +0100

    main eh

---
+++ Equacio/src/com/exemple/eq/CalculPolinomi2nGrau.java
@@ -0,0 +1,23 @@
+package com.exemple.eq;
+
+import java.util.Scanner;
+
+public class CalculPolinomi2nGrau {
+
+	public static void main(String[] args) {
+		
+		int a, b, c;
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Entra a");
+		a = s.nextInt();
+		System.out.println("Entra b");
+		b = s.nextInt();
+		System.out.println("Entra c");
+		c = s.nextInt();
+		
+		Polinomi2nGrau poli = new Polinomi2nGrau(a, b, c);
+		
+	}
+
+}
