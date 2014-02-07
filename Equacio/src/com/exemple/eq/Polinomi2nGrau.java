7dc5046056edbbeb4603d7183e009d3abc109198
NOELL <NOELL@NOELL-PC>
2014-02-07 19:05:30 +0100

    primer equacio

---
+++ Equacio/src/com/exemple/eq/Polinomi2nGrau.java
@@ -0,0 +1,17 @@
+package com.exemple.eq;
+
+public class Polinomi2nGrau {
+	int a;
+	int b;
+	int c;
+	
+	public Polinomi2nGrau(int a, int b, int c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	
+
+}
